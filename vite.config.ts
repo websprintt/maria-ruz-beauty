5cc60f4a805ef9fe172e17dd2dca7fa5724fd757
Websprint <websprintt@gmail.com>
2026-06-06 18:37:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/maria-ruz-beauty/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
